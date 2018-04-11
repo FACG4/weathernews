4c26e100bb0ac1a17f5c3352afefa9742056d485
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-04-11 10:49:55 +0300

    bla bla

--- src/router.js
+++ src/router.js
@@ -5,16 +5,17 @@ const router = (req, res) => {
   if (endpoint === '/') {
     handler.handlePublic(res, path.join('public', 'index.html'));
   } else if (endpoint.includes('public') || endpoint.includes('favicon')) {
-    console.log(endpoint);
     handler.handlePublic (res, endpoint);
   } else if (endpoint === "/city") {
     handler.handleInput(req, res);
   } else if (endpoint === "/news") {
     handler.handleNews(req, res);
   } else if (endpoint === "/weather") {
-    handler.handleWeather();
-  } else {    
+    handler.handleWeather(req, res);
+  } else {
     handler.handleNotFound(req, res);
+    // res.writeHead(404, 'Content-type : text/html');
+    // res.end('<h1>Erorr not found</h1>');
   }
 }
 module.exports = router;
