27b3e475a336a608314100a00b78cc367a8c990a
Abdalsamad Y. Abumusameh <abdalsamad.y.m@gmail.com>
2018-04-11 16:48:04 +0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -20,47 +20,4 @@ t.equal(res.statusCode, 200, 'Should return 200');
 t.end();
 
 });
-})
-
-
-
-test(' handleWeather type of response',(t) => {
-supertest(router)
-.get('/weather')
-.end((err, res) => {
-t.error(err)
- t.equal(typeof res.body,'object','the type of response');
-t.end();
-
 });
-
-});
-
-//
-// test(' handlWeather wind speed',(t) => {
-// supertest(router)
-// .get('/weather')
-// .end((err, res,body) => {
-// t.error(err)
-//  t.equal(res.body.wind.speed, 3.6 ,'the speed of wind');
-// t.end();
-//
-// });
-//
-// });
-
-
-
-
-test('handleWeather status Code',(t) => {
-supertest(router)
-.get('/weather')
-.expect(200)
-// .expect('Content-type', /html/)
-.end((err, res) => {
-t.error(err)
-t.equal(res.statusCode, 200, 'Should return 200');
-t.end();
-
-})
-})
