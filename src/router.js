8f2a88619727bad96a2c101d5826430767a66feb
Abdalsamad Y. Abumusameh <abdalsamad.y.m@gmail.com>
2018-04-11 16:17:43 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -10,13 +10,6 @@ const router = (req, res) => {
     handler.handlePublic (res, endpoint);
   } else if (endpoint === "/city") {
     handler.handleInput(req, res);
-      
-      // (data)=>{
-      // console.log('fgfgfgf',data)
-      // res.end(JSON.stringify(data))
-    // });
-  // } else if (endpoint === "/news") {
-  // } else if (endpoint === "/weather") {
   } else {
     handler.handleNotFound(req, res);
 
